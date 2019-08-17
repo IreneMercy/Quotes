101b102a59510572629e172912babcc70ad2d7d1
IreneMercy <irenemercy700@gmail.com>
2019-08-17 11:28:02 +0300

    add upvote and downvote function

--- src/app/quotes-details/quotes-details.component.ts
+++ src/app/quotes-details/quotes-details.component.ts
@@ -8,7 +8,14 @@ import { Quotes } from '../quotes';
 })
 export class QuotesDetailsComponent implements OnInit {
   @Input() quotes:Quotes;
-  numberOfLikes:number;
+  numberOfLikes:number = 0;
+  numberOfDislike:number = 0;
+  upVote(){
+    this.numberOfLikes++;
+  }
+  downVote(){
+    this.numberOfDislike++;
+  }
   constructor() { }
 
   ngOnInit() {
